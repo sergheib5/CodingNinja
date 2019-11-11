6645e7261308259368c174744d80b70b4693c316
BerfuEren <berfunureren1@gmail.com>
2019-11-11 16:09:29 -0600

    berfu test

--- src/TeamTestProject/TestOneConnect.java
+++ src/TeamTestProject/TestOneConnect.java
@@ -6,8 +6,7 @@ public class TestOneConnect {
     public static void main(String[] args) {
         System.out.println("Serghei connected");
         System.out.println("one");
-        System.out.println("berfu connected");
+        System.out.println("Berfu connected");
         System.out.println("two");
-
     }
 }
