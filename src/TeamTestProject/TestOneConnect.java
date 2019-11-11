28424c56ce62d02b9515e153281cdbcf16e38270
sergheib5 <56229905+sergheib5@users.noreply.github.com>
2019-11-11 16:20:07 -0600

    test1

--- src/TeamTestProject/TestOneConnect.java
+++ src/TeamTestProject/TestOneConnect.java
@@ -4,7 +4,7 @@ import javax.sound.midi.Soundbank;
 
 public class TestOneConnect {
     public static void main(String[] args) {
-        System.out.println("Serghei connected");
+        System.out.println("Serghei connected ");
         System.out.println("one");
         System.out.println("Berfu connected");
         System.out.println("two");
